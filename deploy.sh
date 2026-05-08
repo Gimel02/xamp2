c642876e19031292a2903a15539543847f34ef8e
Gimel02 <alu.22130843@correo.itlalaguna.edu.mx>
2026-05-08 12:35:06 -0600

    Cambio visual

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Subiendo cambios a GitHub..."
+git add .
+git commit -m "Deploy automatico"
+git push origin master
+
 echo "Actualizando repositorio en la instancia..."
 
 ssh -i ../Dock.key ubuntu@160.34.208.187 << 'EOF'
@@ -10,7 +15,7 @@ git pull origin master
 
 echo "Reconstruyendo contenedor..."
 sudo docker-compose -f docker-compose.production.yml down
-sudo docker-compose -f docker-compose.production.yml up -d --build
+sudo docker-compose -f docker-compose.production.yml up -d --build --force-recreate
 
 echo "Contenedores activos:"
 sudo docker ps
